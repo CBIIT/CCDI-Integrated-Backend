25ff391f023d5d9eaeb9cb892d013ceb8f575e7a
Joon Lee <joon.lee2@nih.gov>
2026-07-28 16:07:53 +0000

    Updated unit tests

--- tests/java/gov/nih/nci/ccdi/PortalGraphQLSchemaPresenceTest.java
+++ tests/java/gov/nih/nci/ccdi/PortalGraphQLSchemaPresenceTest.java
@@ -1,22 +1,15 @@
 package gov.nih.nci.ccdi;
 
-import graphql.ExecutionResult;
-import graphql.GraphQL;
 import graphql.language.FieldDefinition;
 import graphql.language.ListType;
 import graphql.language.ObjectTypeDefinition;
 import graphql.language.TypeName;
-import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -84,28 +77,6 @@ class PortalGraphQLSchemaPresenceTest {
                 "the obsolete flat participant type should be removed"
             );
         }
-    }
-
-    @Test
-    void cohortMetadataQueryMatchesPrivateEsSchema() throws Exception {
-        String schemaText;
-        try (InputStream in = classLoader().getResourceAsStream("graphql/ccdi-portal-private-es.graphql")) {
-            assertNotNull(in, "graphql/ccdi-portal-private-es.graphql should be on the classpath");
-            schemaText = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-        }
-
-        TypeDefinitionRegistry registry = new SchemaParser().parse(schemaText);
-        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(
-            registry,
-            RuntimeWiring.newRuntimeWiring().build()
-        );
-        String query = Files.readString(
-            Path.of("doc/cohortMetadata.graphql"),
-            StandardCharsets.UTF_8
-        );
-
-        ExecutionResult result = GraphQL.newGraphQL(schema).build().execute(query);
-        assertTrue(result.getErrors().isEmpty(), result.getErrors().toString());
     }
 
     private static ObjectTypeDefinition objectType(
